c1791f026c1230d024577a9b23bbdab1bcdc9644
Fohte / Hayato Kawai <fohte.hk@gmail.com>
2026-08-24 01:11:51 +0900

    web: Remove viewport workaround for Storybook VRT (#443)

--- web/.storybook/vitest.setup.desktop.ts
+++
@@ -1,8 +0,0 @@
-import { beforeEach } from 'vitest'
-import { page } from 'vitest/browser'
-
-import { DESKTOP_VIEWPORT } from '#storybook-config/screenshot-viewports'
-
-beforeEach(async () => {
-  await page.viewport(DESKTOP_VIEWPORT.width, DESKTOP_VIEWPORT.height)
-})
--- web/.storybook/vitest.setup.mobile.ts
+++
@@ -1,8 +0,0 @@
-import { beforeEach } from 'vitest'
-import { page } from 'vitest/browser'
-
-import { MOBILE_VIEWPORT } from '#storybook-config/screenshot-viewports'
-
-beforeEach(async () => {
-  await page.viewport(MOBILE_VIEWPORT.width, MOBILE_VIEWPORT.height)
-})
